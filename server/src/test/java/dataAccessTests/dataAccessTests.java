0a5b6ad5f2d12b1013e1efa76fa0ed0f6d533073
jeremysummers492 <jeremysummers492@gmail.com>
2024-03-07 21:04:33 -0700

    Updated tests

--- server/src/test/java/dataAccessTests/dataAccessTests.java
+++ server/src/test/java/dataAccessTests/dataAccessTests.java
@@ -1,4 +1,82 @@
 package dataAccessTests;
 
+import dataAccess.*;
+import org.junit.jupiter.api.Test;
+import service.ClearService;
+
 public class dataAccessTests {
+    AuthDAO authDAO = new SQLAuthDAO();
+    UserDAO userDAO = new SQLUserDAO();
+    GameDAO gameDAO = new SQLGameDAO();
+
+    @Test
+    public void gameClear() throws DataAccessException {
+        gameDAO.clear();
+    }
+
+    @Test
+    public void authClear() throws DataAccessException {
+        authDAO.clear();
+    }
+
+    @Test
+    public void userClear() throws DataAccessException {
+        userDAO.clear();
+    }
+
+    @Test
+    public void createAuthTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void getAuthTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void deleteAuthTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void createGameTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void getGameTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void listGamesTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void updateGameTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void verifyColorTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void createUserTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void getUserTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void checkPasswordTest() throws DataAccessException {
+
+    }
+
 }
