790d6718f5dfddc2d7ac50d64fbca7a4b50951b6
jeremysummers492 <jeremysummers492@gmail.com>
2024-03-19 22:27:44 -0600

    Made method declarations for ServerFacade

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,2 +1,29 @@
 public class ServerFacade {
+    public void clear() {
+
+    }
+
+    public void register() {
+
+    }
+
+    public void login() {
+
+    }
+
+    public void logout() {
+
+    }
+
+    public void listGames() {
+
+    }
+
+    public void createGame() {
+
+    }
+
+    public void joinGame() {
+
+    }
 }
